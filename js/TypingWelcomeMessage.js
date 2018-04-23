5393d497fc3d0b43fa66a80205f874e023765adf
Jagjeet Khalsa <Jagjeet.Khalsa@gmail.com>
2018-04-23 00:36:12 -0700

    Delete copy and pasted code from prev project

--- js/TypingWelcomeMessage.js
+++ js/TypingWelcomeMessage.js
@@ -1,11 +1,3 @@
-// Random integer function taken from:
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
-}
-
 const TypingWelcomeMessage = function TypingWelcomeMessage(text)  {
   let typingAnimationTime = 200;
   let msgElem = document.createElement("h1");  
@@ -50,29 +42,7 @@ const TypingWelcomeMessage = function TypingWelcomeMessage(text)  {
 
   document.body.insertBefore(this.resetButton, document.body.nextSibling);
 
-  // let numFrames = 0;
-  // window.requestAnimationFrame(function draw() { 
-
-  //   //Draw circle every 10 frames
-  //   numFrames++;
-  //   if (numFrames % 10 === 1) {
-  //     let r = getRandomIntInclusive(0, 255);
-  //     let g = getRandomIntInclusive(0, 255);
-  //     let b = getRandomIntInclusive(0, 255);
-  //     let x = getRandomIntInclusive(0, 599);
-  //     let y = getRandomIntInclusive(0, 599);
-  //     let a = getRandomIntInclusive(0, 255);
-  //     let radius = getRandomIntInclusive(10, 100);
-
-  //     ctx.fillStyle = "rgba("+r+","+g+","+b+","+(a/255)+")";
-  //     ctx.beginPath();
-  //     ctx.arc(x, y, radius, 0, Math.PI * 2, true); // Outer circle
-  //     ctx.fill();
-  //   } // if
-
-  //   window.requestAnimationFrame(draw);
-  // });
-} //CirclesRandomColorAnimation
+} // TypingWelcomeMessage
 
 
 // Allow inclusion in browers and node
